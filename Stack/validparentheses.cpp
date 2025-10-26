2814bae0a4e1fad8a8182abda317299426084299
ManaswibRane <manaswirane1947@gmail.com>
2025-10-26 18:14:40 +0530

    Added solution to valid parentheses in stack

---
+++ Stack/validparentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+      stack<char> st;
+      if(s.length()==1 && (s[0]=='(' || s[0]=='[' ||s[0]=='{' || s[0]==')' || s[0]==']' ||s[0]=='}')){
+        return false;
+      }
+      for(int i=0;i<s.length();i++){
+       
+           if(s[i]=='(' || s[i]=='[' ||s[i]=='{'  ){
+            st.push(s[i]);
+           }
+            if(st.empty()){
+            return false;
+        }
+           if((s[i]==')' && st.top()=='(') || (s[i]==']' && st.top()=='[') || (s[i]=='}' && st.top()=='{')){
+            st.pop();
+           }
+           else if(s[i]==')' || s[i]==']' || s[i]=='}'){
+            return false;
+           }
+         
+ 
+
+
+      }
+      if(st.empty())
+      {
+        return true;
+      }
+      return false;
+    }
+};
